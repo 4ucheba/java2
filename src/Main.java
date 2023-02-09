98425cbed2ee0c42f0fab467b34f44452571cec8
Yulia Test <4ucheba@mail.ru>
2023-02-09 19:53:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)    {
+
+        int stoimost = 15832;
+        int bonus = 20;
+        int mili = stoimost / bonus;
+
+        System.out.println(mili);
+    }
+}
